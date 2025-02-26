2f0f8a2c7881ae64db2583d90cbd3b2e82f3f4ad
harithaMaram <harithamanimaram04@gmail.com>
2025-02-26 14:56:54 +0530

    Folders structure is created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI',
+    version='0.0.0',
+    author='Haritha',
+    author_email='abc@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
